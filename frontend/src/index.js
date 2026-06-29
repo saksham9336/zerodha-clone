6d8d5aa2126a461ea00df8c3abd2e87ea0caefa0
saksham singh <work.saksham93@gmail.com>
2026-06-29 23:21:36 +0530

    Update frontend entry point

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
 import Signup from './landing_page/signup/Signup';
-import Login from './landing_page/login/login';       // ← YE ADD KARO
+import Login from './landing_page/login/login';       
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/products/ProductPage';
 import PricingPage from './landing_page/pricing/PriceingPage';
@@ -20,7 +20,7 @@ root.render(
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />    {/* ← YE ADD KARO */}
+      <Route path="/login" element={<Login />} />   
       <Route path="/about" element={<AboutPage />} />
       <Route path="/products" element={<ProductPage />} />
       <Route path="/pricing" element={<PricingPage />} />
